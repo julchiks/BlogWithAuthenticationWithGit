7c9eeae68aa59bd04db1ea1f7b8bf11bc6a0fbfa
julchiks <79706949+julchiks@users.noreply.github.com>
2023-08-10 13:35:26 +0100

    some id change

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ class Comment(db.Model):
 
 class User(db.Model, UserMixin):
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username=db.Column(db.String(250), unique=True, nullable=False)
     email=db.Column(db.String(250), unique=True, nullable=False)
     password=db.Column(db.String(20), nullable=False)
